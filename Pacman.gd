a3c32e9196a26383d412117ad415e8ae04e79dbf
BERNARDO LUIZ HAAB <bernardo.luiz@edu.pucrs.br>
2023-10-02 18:51:02 -0300

    Inicio

---
+++ Pacman.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+onready var sprite := $AnimatedSprite
+
+var velocity = Vector2()
+
+func _ready() -> void:
+	velocity.x = speed
+
+func get_side_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	
+
+	if right:
+		inverte_movimento()
+
+func inverte_movimento():
+	velocity.x = velocity.x * -1
+	velocity.y = velocity.y * -1
+
+func _physics_process(delta):
+	get_side_input()
+	velocity.x = speed
+	
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	print(velocity)
