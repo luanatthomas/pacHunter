31b3767c718426634fd36b691936a250c076e5cc
Bernardo Luiz Haab <bernardohaab@gmail.com>
2023-11-24 16:20:25 -0300

    WIP: posiciona serras

--- SerraLinha.gd
+++ SerraLinha.gd
@@ -5,6 +5,8 @@ onready var tween := $Tween
 var posInit : Vector2 = Vector2(0,0)
 var posEnd : Vector2 = Vector2(0,0)
 
+signal pacmanColide
+
 onready var line := $Line2D
 onready var serra := $Serra
 
@@ -18,26 +20,19 @@ func setPointEnd(pointEnd: Vector2):
 	line.add_point(pointEnd)
 
 func startTween():
-	print("SERRA")
-	print(posInit)
-	print(posEnd)
-#	tween.repeat = true
-	tween.interpolate_property($Serra, "position", posInit, posEnd, 1, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
-	tween.interpolate_property($Serra, "rotation", 0, -180, 1, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
+	tween.interpolate_property($Serra, "position", posInit, posEnd, 1.5, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
 	tween.start()
+	
 
-func _process(delta):
+func _process(delta):	
+	serra.rotate(0.3)
 	if !tween.is_active():
 		startTween()
-
-func _on_tween_completed(object, key):
-	if key == ":position":
 		var temp = posEnd
 		posEnd = posInit
 		posInit = temp
 
-		print("ACABOU")
-		print(posInit)
-		print(posEnd)
-		startTween()
-	
+
+func _on_Serra_body_entered(body):
+	if body.name == "Pacman":
+		emit_signal("pacmanColide")
